584fb95cd7e9dae0166e9ad5d0ebd1bd9354e92e
Auros Nexus <41306347+Auros@users.noreply.github.com>
2020-07-05 04:17:23 -0400

    metroapps import and some basic setup

---
+++ SiraOrganizer/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+using SiraOrganizer.Windows;
+
+namespace SiraOrganizer
+{
+    public partial class MainWindow : MetroWindow
+    {
+        private FindBeatSaberDirectory _findBeatSaberDirectory;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FindBeatSaberDir_Click(object sender, RoutedEventArgs e)
+        {
+            if (_findBeatSaberDirectory == null || _findBeatSaberDirectory.IsDisposed)
+            {
+                _findBeatSaberDirectory = new FindBeatSaberDirectory();
+                _findBeatSaberDirectory.Show();
+            }
+            if (!_findBeatSaberDirectory.IsFocused)
+            {
+                _findBeatSaberDirectory.Focus();
+            }
+        }
+    }
+}
---
+++ SiraOrganizer/Windows/FindBeatSaberDirectory.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+using System.ComponentModel;
+
+namespace SiraOrganizer.Windows
+{
+    public partial class FindBeatSaberDirectory : MetroWindow
+    {
+        public bool IsDisposed { get; private set; }
+
+        public FindBeatSaberDirectory()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            IsDisposed = true;
+        }
+    }
+}
